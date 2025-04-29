56dd69edfc7e5d02310064b5b3472c1816e4e051
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-04-29 23:42:38 +0200

    initial commit

--- script.js
+++ script.js
@@ -15,10 +15,14 @@ function addTask() {
   inputBoxEl.value = "";
 }
 
-listContainerEl.addEventListener("click", function (e) {
-  if (e.target.tagName === "LI") {
-    e.target.classList.toggle("checked");
-  } else if (e.target.tagName === "SPAN") {
-    e.target.parentElement.remove();
-  }
-});
+listContainerEl.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
